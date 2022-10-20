a93e0bac1e0d963c1e6b6d70df8ce8dd038f831c
GregW <gregw@backbase.com>
2022-10-20 13:31:58 +0200

    Introducing some errors

--- src/main/java/com/backbase/work/ui/pages/SearchJobPage.java
+++ src/main/java/com/backbase/work/ui/pages/SearchJobPage.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public class SearchJobPage extends BasePage {
 
-    private final By locationDropdownLocator = By.cssSelector(".total__filter--location+i");
-    private final By krakowLocationLocator = By.cssSelector(".job__filter.active .filter:nth-child(1)");
-    private final By functionDropdownLocator = By.cssSelector(".total__filter--subcategory");
-    private final By qaLocationLocator = By.cssSelector(".job__filter.active [value=QA]+span");
-    private final By currentFilterLocation = By.xpath(".//span[@class='currentfilter']");
+    public final By locationDropdownLocator = By.cssSelector(".total__filter--location+i");
+    public final By krakowLocationLocator = By.cssSelector(".job__filter.active .filter:nth-child(1)");
+    public final By functionDropdownLocator = By.cssSelector(".total__filter--subcategory");
+    public final By qaLocationLocator = By.cssSelector(".job__filter.active [value=QA]+span");
+    public final By currentFilterLocation = By.xpath(".//span[@class='currentfilter']");
 
     public SearchJobPage(WebDriver webDriver) {
         super(webDriver);
